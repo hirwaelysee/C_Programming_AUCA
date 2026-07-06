a94a1667cb5b59960c2b5e8153dd3550ea014e68
hirwaelysee <197512064+hirwaelysee@users.noreply.github.com>
2026-07-06 21:53:37 +0200

    Add a simple multiplication table generator system

--- C_Programming_AUCA/7.Multiplication_Table_Generator Nested Loop/Multiplication_Table_Generator.c
+++ C_Programming_AUCA/7.Multiplication_Table_Generator Nested Loop/Multiplication_Table_Generator.c
@@ -1,7 +1,9 @@
 /*
+
 create a Multiplication Table Generator using nested loops in C programming. The
 program will generate the multiplication table for numbers from 1 to a given limit and
 display the results in a structured format.
+
 */ 
 #include<stdio.h>
 int main(){
